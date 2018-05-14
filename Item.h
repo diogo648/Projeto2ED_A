22a19e03df706898a4badbfb572576480512a10b
diogo648 <diogocortez@live.com>
2018-05-14 14:38:46 -0300

    Update Item.h

--- Item.h
+++ Item.h
@@ -2,16 +2,15 @@
 #ifndef ITEM_H_
 #define ITEM_H_
 
-struct tipoItem {
- char chave[50]; /* string que contem a palavra do texto*/
- int valor;  /* Armazena a quantidade de vezes que a palavra aparece  */
-};
-
 typedef struct tipoItem Item;
 
+struct tipoItem {
+ char palavra[50]; /* string que contem a palavra do texto*/
+ int quantidade;  /* Armazena a quantidade de vezes que a palavra aparece  */
+ struct Item *dir,*esq;
+};
 
 void CriarItem();
 void RemoverItem();
-
 
 #endif
