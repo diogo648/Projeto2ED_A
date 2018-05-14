1382de626cba05f4abe99e38c234810d0c9cb501
diogo648 <diogocortez@live.com>
2018-05-14 14:57:18 -0300

    Update Item.c

--- Item.c
+++ Item.c
@@ -23,7 +23,29 @@ void CriarItem(char palavra[50], struct no **raiz){
    (*raiz)-> dir = NULL;
    
    }
-        
+  
+  if(strcmp((*raiz->palavra),palavra) == 0){ /* Compara a raiz com a palavra */
+    
+    (*raiz)->quantidade++; /* Incrementa a quantidade de palavras */ 
+  } 
+  
+  if(strcmp((*raiz->palavra),palavra) < 0 || strcmp((*raiz)->prox->palavra,palavra) != 0 || (*raiz)->prox != NULL){ /* Verifica se a palavra eh maior que a raiz, entra no else se for menor */
+  
+   (*aux) =  (struct no*) malloc (sizeof(struct no));
+ 
+   strcpy(aux-> palavra,palavra); 
+   (*aux)->quantidade = 1;
+   
+   /*Comparar a proxima palavra do no*/
+  } 
+  
+  else{
+  
+  
+  
+  }
+  if() /* Chama a recursão*/
+         
 }
   
   
