079405e1b0dddbbb56cdc25b71bcb60002ca2aa7
diogo648 <diogocortez@live.com>
2018-05-14 14:21:43 -0300

    Update Item.c

--- Item.c
+++ Item.c
@@ -3,8 +3,32 @@
 #include <string.h>
 #include "item.h"
 
-void CriarItem(){
+void CriarItem(char palavra[50], struct no **raiz){
 
+ if(*raiz == NULL){
+ 
+  (*raiz) = (struct no*) malloc (sizeof(struct no));
+   
+   if(*raiz == NULL){
+   
+    printf("\n Erro! Heap Overflow! \n");
+   }
+   
+   else{
+   
+   strcpy((*raiz)->palavra, palavra);
+   (*raiz)->quantidade = 1;
+   
+   (*raiz)-> esq = NULL;
+   (*raiz)-> dir = NULL;
+   
+   }
+   
+}
+  
+  
+  
+  
 }
 
 
