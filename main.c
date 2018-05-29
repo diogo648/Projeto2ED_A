05f1435da6b98c48842c1b2541945592e8cdb171
diogo648 <diogocortez@live.com>
2018-05-29 14:53:17 -0300

    Update main.c

--- main.c
+++ main.c
@@ -14,6 +14,38 @@ Item *arvore = NULL;
 des *desenho = NULL;
 int numeroMaiores;//quantidade de palavras a serem exibidas (escolhido pelo usuario)
 char palavra[50];
+char *n;
+char *fileIn;
+char *fileOut;
+char *word;
+
+  for (int i=1; i< argc; i++) {
+
+    if(strncmp(argv[i],"-n",2) == 0){ //Delimitar o resultado de busca
+
+      n = strtok(argv[i],"-n");
+      numeroMaiores = atoi(n);
+    }
+
+    if(strncmp(argv[i],"-r",2) == 0){ //Carregar informacoes da tabela de simbolos de um arquivo
+
+      fileIn = strtok(argv[i],"-r");
+      printf("%s ",fileIn);
+    }
+
+    if(strncmp(argv[i],"-w",2) == 0){ //Salvar informacoes da tabela de simbolos em um arquivo
+
+       fileOut = strtok(argv[i],"-w");
+       printf("%s ",fileOut);
+    }
+
+     if(strncmp(argv[i],"-s",2) == 0){ //Buscar palavra especifica
+
+       word = strtok(argv[i],"-s");
+       printf("%s ",word);
+    }
+
+  }
 
   while(!feof(stdin)){
      
