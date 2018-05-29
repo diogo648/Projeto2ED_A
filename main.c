193c4e5319bb326e31d076602945428e7188561b
Marcelino150 <37379562+Marcelino150@users.noreply.github.com>
2018-05-29 13:44:31 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,26 +2,37 @@
 #include <stdio.h>
 #include <string.h>
 #include "item.h"
+#include "ST.h"
 
 int main (int argc, char** argv) {
   
-no *lista;
-Item *item; 
+
 //char * parm1; //Parametro1 passado pelo usuario no terminal
-char palavra[20];
-  
+no *lista = NULL;
+Item *item = NULL;
+Item *arvore = NULL;
+des *desenho = NULL;
+int numeroMaiores;//quantidade de palavras a serem exibidas (escolhido pelo usuario)
+char palavra[50];
+
   while(!feof(stdin)){
      
     scanf("%s",palavra);
 
     if(!feof(stdin)){
 
-      criarItem(palavra,&item,0,1);
+      CriarItem(palavra,&item,0,1);//Cria arvore por ordem alfabetica
     }
  
   }
  
-  lista = percorreArv(item,lista);
-  exibeItens(lista);
+  percorreArv(&arvore, &item);//Cria arvore por ordem de quantidade
+  maiores(&arvore,&lista);//Transforma em lista
+  exibeItens(lista,numeroMaiores);//Imprime os mais frequentes
+
+  //imprime(&arvore, NULL, desenho, NULL, (variavel com altura maxima para impressao da arvore aqui));
+  
+  /*lista = percorreArv(item,lista);
+  exibeItens(lista);*/
   
 }
